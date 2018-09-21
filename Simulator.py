82e2d74623fb33f7eab2faa71a89b361d419012e
jtjun <tae7130@naver.com>
2018-09-21 11:44:52 +0900

    한 번에 10개씩

--- Simulator.py
+++ Simulator.py
@@ -207,7 +207,7 @@ class Simulator:
         f.close()
 
 if __name__ == "__main__":
-    n = 1
+    n = 10
     off = False
     for i in range(n) :
         S = Simulator(MapType='clust', ReqType='CS2')
